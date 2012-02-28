830c3abb4cc6ef88c7f5738351ffe2adf61f565d
Michael Seid <mbseid@gmail.com>
2012-02-28 00:02:05 -0500

    Added about page and made a few modifications to the browseSidebar

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import models.Language;
+import models.Paste;
+import models.Version;
 import play.mvc.Controller;
 
 public class Application extends Controller {
@@ -9,6 +12,9 @@ public class Application extends Controller {
     }
 
     public static void about(){
+        renderArgs.put("totalPasteCount", Paste.count());
+        renderArgs.put("languages", Language.findAll());
+        renderArgs.put("versions", Version.findAll());
         render();
     }
 
